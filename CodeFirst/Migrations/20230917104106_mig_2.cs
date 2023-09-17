0b64da69fa579f06c1ce63ca87115314567d9c8c
huseyin corakli <huseyincorakli@icloud.com>
2023-09-17 14:10:37 +0300

    CodeFirst

---
+++ CodeFirst/Migrations/20230917104106_mig_2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeFirst.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MyProperty",
+                table: "ExampleEntities",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MyProperty",
+                table: "ExampleEntities");
+        }
+    }
+}
---
+++ CodeFirst/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+ExampleDbContext context = new ExampleDbContext();
+await context.Database.MigrateAsync();
+
+public class ExampleDbContext : DbContext
+{
+    public DbSet<ExampleEntity> ExampleEntities { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(LocalDb)\\HcSqlServer; Database=ExampleDatabase");
+    }
+}
+
+
+public class ExampleEntity
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int MyProperty { get; set; }
+
+}
+

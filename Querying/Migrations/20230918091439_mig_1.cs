cd95db746dc30d207eff8b7319bd5b5bfc1a5105
huseyin corakli <huseyincorakli@icloud.com>
2023-09-18 13:32:09 +0300

    Querying 1

---
+++ Querying/Migrations/20230918091439_mig_1.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Querying.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Stock = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductComp",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductComp", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductComp_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductComp_ProductId",
+                table: "ProductComp",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductComp");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ Querying/Program.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+ECommerceDbContext context = new();
+
+#region Method Syntax
+
+//var products = await context.Products.ToListAsync();
+
+#endregion
+
+#region Query Syntax
+
+//var products2 = await (from product in context.Products
+//                 select product).ToListAsync();
+
+#endregion
+
+#region IQueryable & IEnumerable
+
+//var products = from product in context.Products
+//               select product;
+//var iqueryable = products;
+//var ienumerable = await products.ToListAsync();
+
+#endregion
+
+#region Deffered Execution
+
+int prouductId = 15;
+var products = from product in context.Products
+               where product.Id > prouductId
+               select product;
+
+prouductId = 250;
+foreach (var product in products)
+{
+    Console.WriteLine(product.Name);
+}
+#endregion
+
+
+public class ECommerceDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(LocalDb)\\HcSqlServer; Database=ExampleDatabase");
+    }
+}
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Stock { get; set; }
+    public ICollection<ProductComp> ProductComps { get; set; }
+
+}
+
+public class ProductComp
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}

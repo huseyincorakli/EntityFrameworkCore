4e2cd6dbe04b875fb7834b362d01cf3b50754d73
huseyin corakli <huseyincorakli@icloud.com>
2023-09-18 12:03:49 +0300

    Data Delete And Persistence

---
+++ Data_Delete_And_Persistence/Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+ECommerceDbContext context = new();
+
+#region contextten çekerek silme
+
+//Product? product = await context.Products.FirstOrDefaultAsync(p => p.Id == 1);
+//if (product != null)
+//{
+//    context.Products.Remove(product);
+//}
+//await context.SaveChangesAsync();
+#endregion
+
+#region track edilmeyen veri nasıl silinir
+//Product product = new Product()
+//{
+//    Id= 3,
+//};
+//context.Products.Remove(product);
+//context.SaveChanges();
+#endregion
+
+#region EntityState üzerinden silme
+//Product product= new Product()
+//{
+//    Id= 2,
+//};
+//context.Products.Entry(product).State=EntityState.Deleted;
+//await context.SaveChangesAsync();
+
+#endregion
+
+#region RemoveRange
+
+List<Product> products= await context.Products.Where(p => p.Id < 8 && p.Id >= 4).ToListAsync();
+
+context.Products.RemoveRange(products);
+await context.SaveChangesAsync();
+
+#endregion
+
+
+public class ECommerceDbContext : DbContext{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(LocalDb)\\HcSqlServer; Database=ExampleDatabase");
+    }
+}
+
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Stock { get; set; }
+}

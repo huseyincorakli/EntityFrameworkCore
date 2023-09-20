bf7feab55b8530aaf3327f7926181824bb503cc4
huseyin corakli <huseyincorakli@icloud.com>
2023-09-20 23:18:47 +0300

    Tracking- 1

---
+++ Tracking/Program.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+ETicaretContext context = new();
+
+
+#region AsNoTracking
+
+var persons = await context.Kullanicilar.AsNoTracking().ToListAsync();
+
+foreach (var person in persons)
+{
+    Console.WriteLine(person.Adi);
+    person.Adi = $"AsNoTracking Kullanıcı-{person.Adi}";
+    context.Kullanicilar.Update(person);
+}
+await context.SaveChangesAsync();
+#endregion
+
+
+#region Context-Entity
+
+
+public class ETicaretContext : DbContext
+{
+    public DbSet<Kullanici> Kullanicilar { get; set; }
+    public DbSet<Rol> Roller { get; set; }
+    public DbSet<Kitap> Kitaplar { get; set; }
+    public DbSet<Yazar> Yazarlar { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(LocalDb)\\HcSqlServer; Database=ExampleDatabase");
+
+    }
+}
+public class Kullanici
+{
+    public int Id { get; set; }
+    public string Adi { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+
+    public ICollection<Rol> Roller { get; set; }
+}
+public class Rol
+{
+    public int Id { get; set; }
+    public string RolAdi { get; set; }
+    public ICollection<Kullanici> Kullanicilar { get; set; }
+}
+public class Kitap
+{
+    public Kitap() => Console.WriteLine("Kitap nesnesi oluşturuldu.");
+    public int Id { get; set; }
+    public string KitapAdi { get; set; }
+    public int SayfaSayisi { get; set; }
+
+    public ICollection<Yazar> Yazarlar { get; set; }
+}
+public class Yazar
+{
+    public Yazar() => Console.WriteLine("Yazar nesnesi oluşturuldu.");
+    public int Id { get; set; }
+    public string YazarAdi { get; set; }
+
+    public ICollection<Kitap> Kitaplar { get; set; }
+}
+#endregion

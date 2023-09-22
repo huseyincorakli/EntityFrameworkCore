60eb9a820b89b552b7c17a2cc1d36c9c30daff2c
huseyin corakli <huseyincorakli@icloud.com>
2023-09-22 15:01:26 +0300

    İlişkisel yapılar - temel kavramlar

---
+++ Relational_Structures/Program.cs
@@ -0,0 +1,14 @@
+﻿
+class Employee
+{
+    public int Id { get; set; }
+    public string EmployeeName { get; set; }
+    public int DepartmantId { get; set; }
+    public Departmant Departmant { get; set; }
+}
+class Departmant
+{
+    public int Id { get; set; }
+    public string DepartmantName { get; set; }
+    public ICollection<Employee> Employees { get; set; }
+}

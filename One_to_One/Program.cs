f79ea08a045a0c8e25a10de02d20f10af0b951b8
huseyin corakli <huseyincorakli@icloud.com>
2023-09-27 19:07:32 +0300

    One to One

---
+++ One_to_One/Program.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+ECommerceDbContext context = new();
+#region Default Convention
+
+//public class Calisan
+//{
+//    public int Id { get; set; }
+//    public string Adi { get; set; }
+//    public CalisanAdres CalisanAdres { get; set; }
+//}
+
+//public class CalisanAdres
+//{
+//    public int Id { get; set; }
+//    public int CalisanId { get; set; }
+//    public string Adres { get; set; }
+//    public Calisan Calisan { get; set; }
+
+//}
+#endregion
+
+#region Data Annotations
+//public class Calisan
+//{
+//    public int Id { get; set; }
+//    public string Adi { get; set; }
+//    public CalisanAdres CalisanAdres { get; set; }
+//}
+
+//public class CalisanAdres
+//{
+//    [Key, ForeignKey(nameof(Calisan))]
+//    public int Id { get; set; }
+//    public string Adres { get; set; }
+//    public Calisan Calisan { get; set; }
+
+//}
+#endregion
+
+#region Fluent API
+public class Calisan
+{
+    public int Id { get; set; }
+    public string Adi { get; set; }
+    public CalisanAdres CalisanAdres { get; set; }
+}
+
+public class CalisanAdres
+{
+
+    public int Id { get; set; }
+    public string Adres { get; set; }
+    public Calisan Calisan { get; set; }
+
+}
+#endregion
+
+#region Context
+public class ECommerceDbContext : DbContext
+{
+    public DbSet<Calisan> Calisanlar { get; set; }
+    public DbSet<CalisanAdres> CalisanAdresleri { get; set; }
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(LocalDb)\\HcSqlServer; Database=ExampleDatabase");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<CalisanAdres>().HasKey(ca => ca.Id);
+
+        modelBuilder.Entity<Calisan>()
+            .HasOne(c => c.CalisanAdres)
+            .WithOne(c => c.Calisan)
+            .HasForeignKey<CalisanAdres>(ca => ca.Id);
+    }
+
+}
+#endregion
--- Relational_Structures/Program.cs
+++ Relational_Structures/Program.cs
@@ -1,14 +1,14 @@
-﻿
-class Employee
-{
-    public int Id { get; set; }
-    public string EmployeeName { get; set; }
-    public int DepartmantId { get; set; }
-    public Departmant Departmant { get; set; }
-}
-class Departmant
-{
-    public int Id { get; set; }
-    public string DepartmantName { get; set; }
-    public ICollection<Employee> Employees { get; set; }
-}
+﻿Console.WriteLine();
+//class Employee
+//{
+//    public int Id { get; set; }
+//    public string EmployeeName { get; set; }
+//    public int DepartmantId { get; set; }
+//    public Departmant Departmant { get; set; }
+//}
+//class Departmant
+//{
+//    public int Id { get; set; }
+//    public string DepartmantName { get; set; }
+//    public ICollection<Employee> Employees { get; set; }
+//}

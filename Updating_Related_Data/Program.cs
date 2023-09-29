e396480ac3e1a31cb07bf2bee72ef7d666a1df34
huseyin corakli <huseyincorakli@icloud.com>
2023-09-29 15:17:30 +0300

    Update

--- Updating_Related_Data/Program.cs
+++ Updating_Related_Data/Program.cs
@@ -93,13 +93,68 @@ Console.WriteLine();
 //await context.SaveChangesAsync();
 #endregion
 #region var olan
-Post? post5 = await context.Posts.FindAsync(5);
-Blog? blog2 = await context.Blogs.FindAsync(2);
-post5.Blog = blog2;
-await context.SaveChangesAsync();
+//Post? post5 = await context.Posts.FindAsync(5);
+//Blog? blog2 = await context.Blogs.FindAsync(2);
+//post5.Blog = blog2;
+//await context.SaveChangesAsync();
 #endregion
 #endregion
 
+#endregion
+#region N-N
+#region Saving
+//Book b1 = new() { BookName = "BOOK 1" };
+//Book b2 = new() { BookName = "BOOK 2" };
+//Book b3 = new() { BookName = "BOOK 3" };
+
+//Author a1 = new() { AuthorName = "AUTHOR 1" };
+//Author a2 = new() { AuthorName = "AUTHOR 2" };
+//Author a3 = new() { AuthorName = "AUTHOR 3" };
+
+//b1.Authors.Add(a1);
+//b1.Authors.Add(a2);
+
+//b2.Authors.Add(a1);
+//b2.Authors.Add(a2);
+//b2.Authors.Add(a3);
+
+//b3.Authors.Add(a3);
+
+//await context.Books.AddRangeAsync(b1, b2, b3);
+//await context.SaveChangesAsync();
+#endregion
+#region 1.Örnek
+////1 idli kitaba 3 idli yazarıda ekleme
+//Book? book = await context.Books.FindAsync(1);
+//Author? author = await context.Authors.FindAsync(3);
+//book.Authors.Add(author);
+//await context.SaveChangesAsync();
+
+
+#endregion
+#region 2.Örnek
+//3 idsine sahip  yazarın 1 idli kitap haricinde diğer kitaplarla ilişiği olmasın
+//Author? author = await context.Authors.Include(a => a.Books).FirstOrDefaultAsync(a => a.Id == 3);
+//foreach (var book in author.Books)
+//{
+//    if (book.Id!=1)
+//    {
+//        author.Books.Remove(book);
+//    }
+//}
+//await context.SaveChangesAsync();
+#endregion
+#region 3.Örnek
+// 2 idsine sahip olan kitabın yazarlarından idsi 2 olanı silip 4. Yazar adlı bir yazar ekle
+//Book? book = await context.Books.Include(b => b.Authors).FirstOrDefaultAsync(b => b.Id == 2);
+//Author? silinecekAuthor= book.Authors.FirstOrDefault(a => a.Id == 2);
+//book.Authors.Remove(silinecekAuthor);
+//book.Authors.Add(new() { AuthorName = "4.Yazar" });
+
+//await context.SaveChangesAsync();
+
+
+#endregion
 #endregion
 class Person
 {

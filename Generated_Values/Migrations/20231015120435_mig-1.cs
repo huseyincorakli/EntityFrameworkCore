8be67ed292379e8e664a59b19d5cbdc4dba0eaec
huseyin corakli <huseyincorakli@icloud.com>
2023-10-15 15:12:49 +0300

    Generated Values - 1

---
+++ Generated_Values/Migrations/20231015120435_mig-1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Generated_Values.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    PersonId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Premium = table.Column<int>(type: "int", nullable: false),
+                    Salary = table.Column<int>(type: "int", nullable: false),
+                    TotalGain = table.Column<int>(type: "int", nullable: true, computedColumnSql: "([Salary]+[Premium]*10)"),
+                    PersonCode = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.PersonId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
---
+++ Generated_Values/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Emit;
+
+ApplicationDbContext context = new();
+
+Person person = new Person()
+{
+	PersonId= 1,
+	Name= "Test",
+	Surname= "Test",
+	TotalGain= 452,
+	Salary=453,
+	PersonCode=Guid.NewGuid(),
+	Premium=123,
+	
+	
+};
+
+await context.Persons.AddAsync(person);
+await context.SaveChangesAsync();
+class Person
+{
+	[DatabaseGenerated(DatabaseGeneratedOption.None)]
+	public int PersonId { get; set; }
+	public string Name { get; set; }
+	public string Surname { get; set; }
+	public int Premium { get; set; }
+	public int Salary { get; set; }
+	[DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+	public int? TotalGain { get; set; }
+	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+	public Guid PersonCode { get; set; }
+}
+
+class ApplicationDbContext : DbContext
+{
+	public DbSet<Person> Persons { get; set; }
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		#region HasDefaultValue & HasDefaultValueSql
+		//modelBuilder.Entity<Person>()
+		//	.Property(p => p.Salary)
+		//	//.HasDefaultValue(100);
+		//	.HasDefaultValueSql("FLOOR(RAND()*1000)");
+		#endregion
+		#region HasComputedColumn
+		modelBuilder.Entity<Person>()
+			.Property(p => p.TotalGain)
+			.HasComputedColumnSql("([Salary]+[Premium]*10)");
+		#endregion
+	}
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		optionsBuilder.UseSqlServer("Server=(LocalDb)\\HcSqlServer; Database=ExampleDatabase");
+	}
+}
